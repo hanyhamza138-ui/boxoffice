ece682dc4bd15e073360138f9b00b84baf9d0710
hanyhamza138-ui <hanyhamza138@gmail.com>
2026-05-24 08:08:05 +0300

    update movie page

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,14 +1,72 @@
-export default function Page() {
+import { supabase } from "../../../lib/supabase";
+
+export default async function Page({ params }) {
+
+  const { data } = await supabase
+    .from("movies")
+    .select("*");
+
+  const movie = data?.find(
+    (m) => String(m.id) === String(params.id)
+  );
+
+  if (!movie) {
+    return (
+      <h1
+        style={{
+          color: "white",
+          background: "black",
+          minHeight: "100vh",
+          padding: 50,
+        }}
+      >
+        Movie not found
+      </h1>
+    );
+  }
+
   return (
-    <h1
+    <main
       style={{
+        background: "#111",
         color: "white",
-        background: "black",
         minHeight: "100vh",
-        padding: 50,
+        padding: 40,
       }}
     >
-      MOVIE PAGE WORKS
-    </h1>
+
+      <img
+        src={movie.poster}
+        alt={movie.title}
+        style={{
+          width: 300,
+          borderRadius: 20,
+          marginBottom: 20,
+        }}
+      />
+
+      <h1
+        style={{
+          fontSize: 50,
+          marginBottom: 20,
+        }}
+      >
+        {movie.title}
+      </h1>
+
+      <p
+        style={{
+          fontSize: 20,
+          marginBottom: 20,
+        }}
+      >
+        {movie.description}
+      </p>
+
+      <h2>
+        💰 Revenue: ${movie.revenue}
+      </h2>
+
+    </main>
   );
 }
