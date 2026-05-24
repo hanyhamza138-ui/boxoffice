9b2ce8b50ef079f4c5fc7c7e2a5149a759416502
hanyhamza138-ui <hanyhamza138@gmail.com>
2026-05-24 07:57:39 +0300

    test route

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,74 +1,14 @@
-import { supabase } from "../../../lib/supabase";
-
-export default async function MoviePage({ params }) {
-
-  const id = Number(params.id);
-
-  const { data: movie, error } = await supabase
-    .from("movies")
-    .select("*")
-    .eq("id", id)
-    .single();
-
-  if (error || !movie) {
-    return (
-      <main
-        style={{
-          background: "#111",
-          color: "white",
-          minHeight: "100vh",
-          padding: 40,
-        }}
-      >
-        <h1>Movie not found</h1>
-      </main>
-    );
-  }
-
+export default function Page() {
   return (
-    <main
+    <h1
       style={{
-        background: "#111",
         color: "white",
+        background: "black",
         minHeight: "100vh",
-        padding: 40,
+        padding: 50,
       }}
     >
-
-      <img
-        src={movie.poster}
-        alt={movie.title}
-        style={{
-          width: 300,
-          borderRadius: 20,
-          marginBottom: 20,
-        }}
-      />
-
-      <h1
-        style={{
-          fontSize: 50,
-          marginBottom: 20,
-        }}
-      >
-        {movie.title}
-      </h1>
-
-      <p
-        style={{
-          fontSize: 20,
-          marginBottom: 20,
-        }}
-      >
-        {movie.description}
-      </p>
-
-      <h2>
-        💰 Revenue:
-        {" "}
-        ${movie.revenue}
-      </h2>
-
-    </main>
+      MOVIE PAGE WORKS
+    </h1>
   );
 }
