90e6ec900f6698c63b578f7e41cb5e70bc536dff
hanyhamza138-ui <hanyhamza138@gmail.com>
2026-06-18 09:35:17 +0300

    fix Adminstats cinema action import

--- app/admin/Adminstats/cinemas/page.js
+++ app/admin/Adminstats/cinemas/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { supabase } from "../../../lib/supabase";
-import { deleteCinema } from "../../actions";
+import { supabase } from "../../../../lib/supabase";
+import { deleteCinema } from "../../../../actions";
 
 export default async function CinemasPage() {
 
